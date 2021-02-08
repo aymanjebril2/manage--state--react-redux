c87a9a7974185cfca4a2fa327ba7c626117aab66
Ayman Omer <aymangebrilomer@gmail.com>
2021-02-08 00:58:47 -0500

    done from the homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addBird, incrementBird } from "./store/birds/birds";
 
 function App() {
+  const [birdName, setBird] = useState("");
+  const birds = [...useSelector((state) => state.birds)].sort((a, b) => {
+    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+  });
+  const dispatch = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addBird(birdName));
+    setBird("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Bird List</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Add Bird</p>
+          <input
+            type="text"
+            onChange={(e) => setBird(e.target.value)}
+            value={birdName}
+          />
+        </label>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <ul>
+        {birds.map((bird) => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>
+              Views: {bird.views}
+              <button onClick={() => dispatch(incrementBird(bird.name))}>
+                <span role="img" aria-label="add">
+                  ➕
+                </span>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
